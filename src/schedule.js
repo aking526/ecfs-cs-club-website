1de59e119404c46f42ae0da84cbb4b87d9555a0a
aking526 <asking52607@gmail.com>
2023-05-16 15:04:30 -0400

    updates to frontend

--- src/schedule.js
+++ src/schedule.js
@@ -1,4 +1,12 @@
+import fs from "fs";
+import { csvToArray } from "./utils";
+
 export function setupSchedule(element) {
+  const file = fs.readFileSync("../files/schedule.csv").toString();
+  const data = csvToArray(file); 
+
+  console.log(data);
+
   element.innerHTML = `
     <h2>Schedule</h2>
   `;
